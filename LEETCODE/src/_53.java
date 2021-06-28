50b93aa342c03553458a5ce0afdc5e89a6e8382e
vardh <vardh@DESKTOP-1PIO2V5>
2021-06-28 23:27:31 +0530

    Maximum_Subarray

---
+++ LEETCODE/src/_53.java
@@ -0,0 +1,19 @@
+
+public class _53 {
+    public int maxSubArray(int[] nums) {
+        int max_sum=nums[0];
+        int cur_sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            cur_sum=cur_sum+nums[i];
+            if(cur_sum>max_sum)
+                max_sum=cur_sum;
+            if(cur_sum<0)
+                cur_sum=0;
+                
+}
+        return max_sum;
+        
+    }
+}
+
